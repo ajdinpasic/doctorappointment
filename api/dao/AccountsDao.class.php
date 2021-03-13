0858d5e4c498bb2d5d1afbc69b1215506c7f3f25
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-13 12:45:57 +0100

    defining addAccount AccountByEmail AccountByEmail in AcccountsDao

--- api/dao/AccountsDao.class.php
+++ api/dao/AccountsDao.class.php
@@ -4,15 +4,32 @@ require_once dirname(__FILE__)."/BaseDao.class.php";
 
   class AccountsDao extends BaseDao{
 
-    public function getUserById($id) {
+    public function getAccountById($id) {
       return $this->query_unique("SELECT * FROM accounts WHERE account_id = :account_id",["account_id"=>$id]);
     }
     /*public function addAccount($account) {
 
     } */
-    public function getUserByType($type) {
-      return $this->query("SELECT * FROM accounts WHERE type = :type",["type"=>$type]);
+
+    public function getAccountbyEmail($email) {
+      return $this->query_unique("SELECT * FROM accounts WHERE email = :email",["email"=>$email]);
     }
+
+    public function getAccountByType($type) {
+
+      return $this->query("SELECT * FROM accounts where type = :type",["type"=>$type]);
+
+    }
+
+    public function addAccount($account) {
+      $sql = "INSERT INTO accounts (created_at, type, password, email) VALUES (:created_at, :type, :password, :email)";
+      $stmt= $this->connection->prepare($sql);
+      $stmt->execute($account);
+
+
+    }
+
+
 
   }
 
