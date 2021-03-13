6453a54426eeea3c2ef8ba8ac51968b9d4d43465
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-13 12:48:00 +0100

    testing previous functions

--- api/test.php
+++ api/test.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
 
 
 
-$patients_dao=new PatientsDao();
+/* $patients_dao=new PatientsDao();
 
 $patient=$patients_dao->getPatientByEmail("m.k@gmail.com");
 print_r ($patient);
@@ -18,7 +18,30 @@ print_r ($patient);
 
 $account_dao=new AccountsDao();
 $account=$account_dao->getUserById(1);
-print_r ($account);
+print_r ($account);  */
+
+/*
+$acc1=new AccountsDao();
+
+$r1_acc1=$acc1->getAccountByType("patient");
+
+print_r ($r1_acc1); */
+/*
+$account_dao=new AccountsDao();
+
+$acc1= [
+
+  "created_at" => "2021-03-13 11:37:58",
+  "type" =>"paient",
+  "password" => "bv9",
+  "email" => "naida.f@gmail.com"
+
+];
+
+$account_dao->addAccount($acc1);
+
+ print_r ($acc1); */
+
 
 
 
