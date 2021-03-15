334c98eb30c2596356fa582514e1243b7e63918a
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-15 23:45:19 +0100

    adding getAllCredit & getCardById

---
+++ api/dao/CreditCards.class.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once dirname (__FILE__)./"BaseDao.class.php";
+
+class CreditCardDao extends BaseDao {
+
+    public function getAllCreditCards() {
+      return $this->query("SELECT * FROM creditcards",[]);
+    }
+    public function getCreditCardbyId($id) {
+      return $this->query_unique("SELECT * FROM creditcards WHERE credit_card_id = :id",["id" => $id]);
+    }
+}
+
+
+
+
+
+ ?>
