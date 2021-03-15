0255369ef9358bf47d1d5379d7e76c6f4b9f0b94
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-15 23:49:56 +0100

    adding addCard

--- api/dao/CreditCards.class.php
+++ api/dao/CreditCards.class.php
@@ -13,6 +13,9 @@ class CreditCardDao extends BaseDao {
     public function getCreditCardbyId($id) {
       return $this->query_unique("SELECT * FROM creditcards WHERE patient_id = :id",["id" => $id]);
     }
+    public function addCreditCard($card) {
+      return $this->insert("creditcards",$card);
+    }
 }
 
 
