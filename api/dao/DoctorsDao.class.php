c231a9d3ba5102e9b9cd4b4b6d354eeb7540f54f
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-15 21:17:42 +0100

    addDoctor in doctorsDao

--- api/dao/DoctorsDao.class.php
+++ api/dao/DoctorsDao.class.php
@@ -14,7 +14,12 @@ require_once dirname(__FILE__)."/BaseDao.class.php";
   public function getDoctorById($id) {
     return $this->query_unique("SELECT * FROM doctors WHERE doctor_id = :id",["id"=>$id]);
   }
-
+  public function updateDoctorById($id,$doctor) {
+    return $this->update($id,"doctors",$doctor,"doctor_id");
+  }
+  public function addDoctor($doctor) {
+    return $this->insert("doctors",$doctor);
+  }
 
   }
 
