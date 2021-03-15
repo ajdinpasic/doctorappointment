e0f4d111ff568a9370df1f6d49328e003ca69ea8
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-15 21:11:58 +0100

    getDoctorById in doctorDao

--- api/dao/DoctorsDao.class.php
+++ api/dao/DoctorsDao.class.php
@@ -1,13 +1,19 @@
 <?php
 
+require_once dirname(__FILE__)."/BaseDao.class.php";
+
   class DoctorsDao extends BaseDao{
 
   /*  public function getDoctorbyId($id) {
 
   } */
 
-
-
+  public function getAllDoctors() {
+    return $this->query("SELECT * FROM doctors",[]);
+  }
+  public function getDoctorById($id) {
+    return $this->query_unique("SELECT * FROM doctors WHERE doctor_id = :id",["id"=>$id]);
+  }
 
 
   }
