045473e3406a7adb0af3da40957f68916a569015
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-16 10:42:24 +0100

    rearranging methods AppDet dao

--- api/dao/AppointmentDetailsDao.class.php
+++ api/dao/AppointmentDetailsDao.class.php
@@ -3,9 +3,13 @@ require_once dirname(__FILE__)."/BaseDao.class.php";
 
 class AppointmentDetailsDao extends BaseDao {
 
+  public function __construct() {
+    parent:: __construct(""appointmentdetails"");
+  }
+
       public function getAllDetails() {
         return $this->query("SELECT * FROM appointmentdetails",[]);
-      }
+      } /*
       public function getDetailByAppointmentId($id) {
         return $this->query_unique("SELECT * FROM appointmentdetails WHERE appointment_id = :id",["id" => $id]);
       }
@@ -14,7 +18,7 @@ class AppointmentDetailsDao extends BaseDao {
       }
       public function updateDetailsByAppointmentId($id,$detail) {
         $this->update($id,"appointmentdetails",$detail,"appointment_id");
-      }
+      } */
 
 
 
