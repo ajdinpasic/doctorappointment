eeb04966310bdb6fe110ee67622a4f3a49bdf946
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-16 11:52:28 +0100

    commenting out unnecessary methods in APPDET DAO

--- api/dao/AppointmentDetailsDao.class.php
+++ api/dao/AppointmentDetailsDao.class.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__)."/BaseDao.class.php";
 class AppointmentDetailsDao extends BaseDao {
 
   public function __construct() {
-    parent:: __construct("appointmentdetails");
+    parent:: __construct("appointmentdetails","app_detail_id");
   }
 
       public function getAllDetails() {
