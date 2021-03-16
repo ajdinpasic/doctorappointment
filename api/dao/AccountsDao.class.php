611cf0478f04a013c270d9da4bfb79808985e419
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-16 11:48:40 +0100

    commenting out unnecessary methods ACCDAO

--- api/dao/AccountsDao.class.php
+++ api/dao/AccountsDao.class.php
@@ -7,21 +7,32 @@ require_once dirname(__FILE__)."/BaseDao.class.php";
   class AccountsDao extends BaseDao{
 
     public function __construct() {
-        parent::__construct("accounts");
+        parent::__construct("accounts","account_id");
     }
-
- /*
-    public function getAccountById($id) {
-      return $this->query_unique("SELECT * FROM accounts WHERE account_id = :account_id",["account_id"=>$id]);
+    public function updateAccountbyEMail($email,$account) {
+      $this->update($email,"accounts",$account,"email");
     }
-    /*public function addAccount($account) {
-
+    public function getAllAccounts() {
+      return $this->query("SELECT * FROM accounts",[]);
     }
-
     public function getAccountbyEmail($email) {
       return $this->query_unique("SELECT * FROM accounts WHERE email = :email",["email"=>$email]);
     }
+  }
+ /*
+     public function getAllAccounts() {
+       return $this->query("SELECT * FROM accounts",[]);
+     }
 
+    public function getAccountById($id) {
+      return $this->query_unique("SELECT * FROM accounts WHERE account_id = :account_id",["account_id"=>$id]);
+    }
+    public function addAccount($account) {
+
+    }
+    */
+
+    /*
     public function addAccount($account) {
 
       return $this->insert("accounts",$account);
@@ -30,16 +41,7 @@ require_once dirname(__FILE__)."/BaseDao.class.php";
     public function updateAccountById($account_id,$account) {
 
       $this->update($account_id,"accounts",$account,"account_id");
-    }
-    public function updateAccountbyEMail($email,$account) {
-      $this->update($email,"accounts",$account,"email");
     } */
-    public function getAllAccounts() {
-      return $this->query("SELECT * FROM accounts",[]);
-    }
-
-  }
-
 
 
 ?>
