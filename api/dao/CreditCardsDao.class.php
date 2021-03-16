df63ec87967af1f207deba9252c1f6e890e55d78
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-16 11:57:02 +0100

    commenting out unnecessary methods in creditcards DAO

--- api/dao/CreditCardsDao.class.php
+++ api/dao/CreditCardsDao.class.php
@@ -3,19 +3,22 @@
 require_once dirname (__FILE__)."/BaseDao.class.php";
 
 class CreditCardDao extends BaseDao {
-
+  public function __construct() {
+      parent::__construct("creditcards","credit_card_id");
+  }
+    public function getCreditCardbyPatientId($id) {
+      return $this->query_unique("SELECT * FROM creditcards WHERE patient_id = :id",["id" => $id]);
+    } /*
     public function getAllCreditCards() {
       return $this->query("SELECT * FROM creditcards",[]);
     }
     public function getCreditCardbyId($id) {
       return $this->query_unique("SELECT * FROM creditcards WHERE credit_card_id = :id",["id" => $id]);
     }
-    public function getCreditCardbyPatientId($id) {
-      return $this->query_unique("SELECT * FROM creditcards WHERE patient_id = :id",["id" => $id]);
-    }
+
     public function addCreditCard($card) {
       return $this->insert("creditcards",$card);
-    }
+    } */
 }
 
 
