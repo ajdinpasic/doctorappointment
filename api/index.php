85a1d30bef0055410973456ec6c487e1588bad59
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-18 16:49:47 +0100

    adding flights for add,update,get & getAll accounts in index.php

--- api/index.php
+++ api/index.php
@@ -5,17 +5,51 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once dirname(__FILE__)."/dao/BaseDao.class.php";
+require_once dirname(__FILE__)."/dao/AccountsDao.class.php";
 require dirname (__FILE__).'/../vendor/autoload.php';
 
-//$baseObject=new BaseDao();
+
+Flight::register('account', 'AccountsDao');
+
+
 
 Flight::route('/', function(){
     echo 'hello world!';
 });
 
-Flight::route('/testiram', function(){
+Flight::route('GET/testiram', function(){
     echo 'flight php testing';
 });
+
+Flight::route('GET /accounts', function(){
+    $result=Flight::account()->getAllEntities();
+    Flight::json($result);
+
+});
+Flight::route('GET /accounts/@id', function($id){
+    $result=Flight::account()->getEntity($id);
+    Flight::json($result);
+
+});
+
+Flight::route('POST /accounts', function(){
+  $request=Flight::request();
+  $data=$request->data->getData();
+  $result=Flight::account()->insertEntity($data);
+
+  Flight::json($result);
+
+});
+
+Flight::route('PUT /accounts/@id', function($id){
+  $request=Flight::request();
+  $data=$request->data->getData();
+  Flight::account()->updateEntity($id,$data);
+  $result=Flight::account()->getEntity($id);
+  Flight::json($result);
+
+});
+
 
 Flight::start();
 
