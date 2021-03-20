2478fa0979ed96611154a99ebab12839f8aecaa2
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-03-20 15:51:07 +0100

    updating patient routes

--- api/routes/PatientRoutes.php
+++ api/routes/PatientRoutes.php
@@ -1,14 +1,37 @@
 <?php
 
+require_once dirname(__FILE__)."/../dao/BaseDao.class.php";
+
+
 Flight::route('GET /patients', function(){
   $offset= Flight::routeForLimitAndOffset("offset",0);
   $limit= Flight::routeForLimitAndOffset("limit",10);
   $search = Flight::routeForLimitAndOffset("search");
 
-  Flight::json(Flight::patient_service()->getAccountService($search,$offset,$limit));
+  Flight::json(Flight::patient_service()->getPatientService($search,$offset,$limit));
 
 });
 
+Flight::route('GET /patient/@id', function($id){
+  $result=Flight::patient_dao()->getEntity($id);
+  Flight::json($result);
+});
+
+Flight::route('POST /patients', function(){
+  $request = Flight::request();
+  $data=$request->data->getData();
+  $result=Flight::patient_dao()->insertEntity($data);
+  Flight::json($result);
+});
+
+Flight::route('PUT /patients/@id', function($id){
+  $request=Flight::request();
+  $data=$request->data->getData();
+  Flight::patient_dao()->updateEntity($id,$data);
+  $result=Flight::patient_dao()->getEntity($id);
+  Flight::json($result);
+
+});
 
 
 
