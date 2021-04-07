0d4b67cc042b654c6a51dd0414a78a8312ce91ff
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-04-08 01:37:43 +0200

    adding appointment routes for doctor and patient

--- api/routes/AppointmentRoutes.php
+++ api/routes/AppointmentRoutes.php
@@ -6,13 +6,26 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
+/**
+ * @OA\Post(path="/patients/appointments/register", tags={"appointments"},security={{"ApiKeyAuth":{}}},
+ *   @OA\RequestBody(description="Appointment is registered by patient", required=true,
+ *       @OA\MediaType(mediaType="application/json",
+ *    			@OA\Schema(
+ *    				 @OA\Property(property="scheduled_at", required="true", type="timestamp", example="2021-03-13 11:37:58",	description="Timestamp of the appointment" ),
+ *     				 @OA\Property(property="office", required="true", type="integer", example="10",	description="Number of the office" ),
+ *    				 @OA\Property(property="doctor_name", required="true", type="string", example="1",	description="Name of the doctor" )
+ *          )
+ *       )
+ *     ),
+ *  @OA\Response(response="200", description="New appointment is registered")
+ * )
+ */
 
-
-Flight::route('POST /appointments/register', function(){
+Flight::route('POST /patients/appointments/register', function(){
   $request = Flight::request();
   $data=$request->data->getData();
-  $result=Flight::appointment_service()->insertEntity($data);
-  Flight::json($result);
+  $result=Flight::appointment_service()->register(Flight::get("patient")["patient_id"],$data);
+  Flight::json("Appointment successfully scheduled");
 });
 
 
@@ -33,13 +46,50 @@ Flight::route('GET /appointment', function(){
   Flight::json($result);
 });
 
+/**
+ * @OA\Get(
+ *     path="/patients/appointments/{appointment_id}",tags={"appointments"},security={{"ApiKeyAuth":{}}},
+ *@OA\Parameter(
+ *    type="integer",
+ *    in="path",
+ *    allowReserved=true,
+ *    name="appointment_id",
+ *    example="1"),
+ *     @OA\Response(response="200", description="Get specific appointment based on given id")
+ *
+*
+* )
+ */
 
-
-Flight::route('GET /appointment/@id', function($id){
-  $result=Flight::appointment_service()->getEntity($id);
+Flight::route('GET /patients/appointments/@id', function($id){
+  $result=Flight::appointment_service()->getAppointmentForPatient(Flight::get("patient")["patient_id"],$id);
   Flight::json($result);
 });
 
+
+
+
+/**
+ * @OA\Get(
+ *     path="/doctors/appointments/{appointment_id}",tags={"appointments"},security={{"ApiKeyAuth":{}}},
+ *@OA\Parameter(
+ *    type="integer",
+ *    in="path",
+ *    allowReserved=true,
+ *    name="appointment_id",
+ *    example="1"),
+ *     @OA\Response(response="200", description="Get specific appointment based on given id")
+ *
+*
+* )
+ */
+
+Flight::route('GET /doctors/appointments/@id', function($id){
+  $result=Flight::appointment_service()->getAppointmentForDoctor(Flight::get("doctor")["doctor_id"],$id);
+  Flight::json($result);
+});
+
+/*
 Flight::route('PUT /appointment/@id', function($id){
   $request=Flight::request();
   $data=$request->data->getData();
@@ -47,6 +97,6 @@ Flight::route('PUT /appointment/@id', function($id){
   $result=Flight::appointment_service()->getEntity($id);
   Flight::json($result);
 
-});
+}); */
 
  ?>
