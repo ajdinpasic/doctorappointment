38e3ef1e4d7f9d63318b6be8533af6307d62319c
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-05-13 16:59:40 +0200

    expanding middleware for newly created routes

--- api/routes/middleware.php
+++ api/routes/middleware.php
@@ -39,7 +39,7 @@ Flight::route('/doctors/*', function(){
 
    $doctor = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
     if (Flight::request()->method != "GET" && (!isset($doctor["doctor_id"]))){
-      throw new Exception("Patients are not allowed to perform that", 403);
+      throw new Exception("You are not allowed to perform that", 403);
     }
     Flight::set('doctor', $doctor);
     return TRUE;
@@ -49,6 +49,8 @@ Flight::route('/doctors/*', function(){
     die;
   }
 });
+
+
 Flight::route('/patients/*', function(){
   if (Flight::request()-> url == "/swagger") return TRUE;
   if(str_starts_with(Flight::request()-> url, "/patients/register")) return TRUE;
@@ -62,8 +64,8 @@ Flight::route('/patients/*', function(){
 
   try {
     $patient = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
-     if (Flight::request()->method != "GET" && (!isset($patient["patient_id"]))){
-       throw new Exception("Doctors are not allowed to perform that", 403);
+     if (Flight::request()->method != "GET" && (!isset($patient["patient_id"]))) {
+       throw new Exception("You are not allowed to perform that", 403);
      }
      Flight::set('patient', $patient);
      return TRUE;
@@ -72,12 +74,78 @@ Flight::route('/patients/*', function(){
     Flight::json(["message" => $e->getMessage()], 401);
     die;
   }
+  });
 
-});
+  Flight::route('/doctor/appointments/*', function(){
+    if (Flight::request()-> url == "/swagger") return TRUE;
+
+
+    $headers= getallheaders();
+    $token= @$headers["Authentication"];
+
+    try {
+      $doctor = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
+       if (!isset($doctor["doctor_id"])) {
+         throw new Exception("This is not for you", 403);
+       }
+       Flight::set('doctor', $doctor);
+       return TRUE;
+
+    } catch (Exception $e) {
+      Flight::json(["message" => $e->getMessage()], 401);
+      die;
+    }
+    });
+
+
+    Flight::route('/patient/appointments/*', function(){
+      if (Flight::request()-> url == "/swagger") return TRUE;
+
+
+      $headers= getallheaders();
+      $token= @$headers["Authentication"];
+
+      try {
+        $doctor = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
+         if (!isset($doctor["doctor_id"])) {
+           throw new Exception("This is not for you", 403);
+         }
+         Flight::set('doctor', $doctor);
+         return TRUE;
+
+      } catch (Exception $e) {
+        Flight::json(["message" => $e->getMessage()], 401);
+        die;
+      }
+      });
 
 
 
-Flight::route('/patients/appointments/*', function(){
+
+Flight::route('/doctors/appointment/@id', function($id){
+    if (Flight::request()-> url == "/swagger") return TRUE;
+
+
+    $headers= getallheaders();
+    $token= @$headers["Authentication"];
+    //print_r($headers); die;
+    try {
+
+     $doctor = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
+      if (!isset($doctor["doctor_id"])){
+        throw new Exception("You cannot access this", 403);
+      }
+      Flight::set('doctor', $doctor);
+      return TRUE;
+
+    } catch (Exception $e) {
+      Flight::json(["message" => $e->getMessage()], 401);
+      die;
+    }
+  });
+
+
+Flight::route('/patients/appointment/@id', function($id){
   if (Flight::request()-> url == "/swagger") return TRUE;
 
   $headers= getallheaders();
@@ -98,26 +166,115 @@ Flight::route('/patients/appointments/*', function(){
 
 });
 
-Flight::route('/doctors/appointments/*', function(){
+Flight::route('/patients/@id', function($id){
   if (Flight::request()-> url == "/swagger") return TRUE;
-
+  if(str_starts_with(Flight::request()-> url, "/patients/register")) return TRUE;
+  if(str_starts_with(Flight::request()-> url, "/patients/login")) return TRUE;
+  if(str_starts_with(Flight::request()-> url, "/patients/forget")) return TRUE;
+  if(str_starts_with(Flight::request()-> url, "/patients/reset")) return TRUE;
+  if(str_starts_with(Flight::request()-> url, "/patients/confirm")) return TRUE;
 
   $headers= getallheaders();
   $token= @$headers["Authentication"];
-  //print_r($headers); die;
-  try {
 
-   $doctor = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
-    if (!isset($doctor["doctor_id"])){
-      throw new Exception("You cannot access this", 403);
-    }
-    Flight::set('doctor', $doctor);
-    return TRUE;
+  try {
+    $patient = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
+     if (isset($patient["patient_id"]) && $patient["patient_id"]!= $id ) {
+       throw new Exception("This is not for you", 403);
+     }
+     Flight::set('patient', $patient);
+     return TRUE;
 
   } catch (Exception $e) {
     Flight::json(["message" => $e->getMessage()], 401);
     die;
   }
-});
+  });
+
+  Flight::route('/patients/', function(){
+    if (Flight::request()-> url == "/swagger") return TRUE;
+
+
+    $headers= getallheaders();
+    $token= @$headers["Authentication"];
+
+    try {
+      $patient = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
+       if (isset($patient["patient_id"])) {
+         throw new Exception("This is not for you", 403);
+       }
+       Flight::set('patient', $patient);
+       return TRUE;
+
+    } catch (Exception $e) {
+      Flight::json(["message" => $e->getMessage()], 401);
+      die;
+    }
+    });
+
+
+    Flight::route('/doctor/details/*', function(){
+          if (Flight::request()-> url == "/swagger") return TRUE;
+
+
+          $headers= getallheaders();
+          $token= @$headers["Authentication"];
+
+          try {
+            $doctor = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
+             if (!isset($doctor["doctor_id"])) {
+               throw new Exception("This is not for you", 403);
+             }
+             Flight::set('doctor', $doctor);
+             return TRUE;
+
+          } catch (Exception $e) {
+            Flight::json(["message" => $e->getMessage()], 401);
+            die;
+          }
+          });
+
+
+          Flight::route('/patient/details/*', function(){
+                if (Flight::request()-> url == "/swagger") return TRUE;
+
+
+                $headers= getallheaders();
+                $token= @$headers["Authentication"];
+
+                try {
+                  $patient = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
+                   if (!isset($patient["patient_id"])) {
+                     throw new Exception("This is not for you", 403);
+                   }
+                   Flight::set('patient', $patient);
+                   return TRUE;
+
+                } catch (Exception $e) {
+                  Flight::json(["message" => $e->getMessage()], 401);
+                  die;
+                }
+                });
+
+                Flight::route('/payments/*', function(){
+                      if (Flight::request()-> url == "/swagger") return TRUE;
+
+
+                      $headers= getallheaders();
+                      $token= @$headers["Authentication"];
+
+                      try {
+                        $patient = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, ["HS256"]);
+                         if (!isset($patient["patient_id"])) {
+                           throw new Exception("This is not for you", 403);
+                         }
+                         Flight::set('patient', $patient);
+                         return TRUE;
+
+                      } catch (Exception $e) {
+                        Flight::json(["message" => $e->getMessage()], 401);
+                        die;
+                      }
+                      });
 
  ?>
