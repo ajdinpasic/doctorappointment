a527d3019b0b6fcab25162b1ce43527ffa972a44
ajdinpasic <ajdin.pasic@stu.ibu.edu.ba>
2021-05-27 12:24:38 +0200

    adding method for decoding jwt token

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -10,3 +10,13 @@ function jsonize_form(selector) {
   }console.log(data);
   return form_data;
 }
+
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
